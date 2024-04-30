e5be9e8da33d236e707461947d7079682bf5b20f
E3A50”git config --global user.name E3A50” <u11002150@o365.ttu.edu.tw>
2024-04-30 20:44:35 +0800

    第六次上傳，調整排版、註解，修正一、二題，完成第六題 心得:這次很特別的多學習了一個工具，是github，雖然有點生疏，但在助教的教學下，順利的利用這個工具做一些簡單的操作，回到這個程式作業的內容，雖然以前有寫過類似的題目，但還是要重寫練習一次，還是有卡住的部分，像是第三題的三角形輸出，要控制順序與排列，也是需要良好的邏輯思考，在想不出來的時候，我選擇先跳過，鮮血其他題，這個作業有個特點，就是一個大程式裡，後半都是一個一個小的內容，去構建的，相互沒有相關性，所以可以先寫其他的部分，有想法得先完成，在回頭來處理這題搞不定的內容，我也回去翻翻課本，查察上課講義，看看有沒有甚麼線索，可以利用，最後在不斷嘗試後，終於試成功了，順利的試出題目要的輸出表示，在做完全部得的內容後，像是完成了一項任務與專案，真是令我充滿成就感。也複習了課本第7章的內容。 太讚了~

--- HW2.c
+++ HW2.c
@@ -2,12 +2,144 @@
 #include<stdlib.h> 	
 int main (void)
 {	
-	char c,A,B;//case A的變數 
-	int space;//case A的空格變數 
+	//副程式宣告
+	void menu(void); //主選單畫面
+	void picture(void); //個人風格畫面 
+	
+	//變數宣告
 	int count = 0,password = 2023 ;//第一題用的計數跟密碼 
-	char MENU;//主選單的輸入 
-	int i,j,n;// case B的變數 
-	char OPTION;//case C的變數 
+	char MENU;		//主選單的輸入變數 	
+	char c,A,B;		//case A的變數 
+	int space;		//case A的空格變數 
+	int i,j,n;		// case B的變數 
+	char OPTION;	//case C的變數 
+	
+	/*程式內容如下*/ 
+	picture();		//呼叫副程式，輸出個人風格畫面 
+	
+	while (count < 3) //計數輸入次數 
+	{
+        printf("請輸入四個數字的密碼：");
+        scanf("%d",&password);
+        count++;				//計數+1 
+        if (password==2023)		//判斷密碼 
+		{
+            printf("輸入正確！歡迎~\n");
+            break;
+        } 
+		else 
+		{
+	        printf("密碼錯誤，已輸入: %d 次。\n", count);
+            if (count == 3) 
+			{
+        		printf("輸入已連續錯誤三次，登入結束！\n");
+      		  printf("%c", '\a');					//輸出警告聲響 
+				return 0;
+    		}
+    	}
+    }
+//_________________________________以上為第一題	
+	system("CLS");               // 清除螢幕
+
+	while(1)//重複執行 
+	{
+		menu();//召喚副程式 印出主選單 
+		printf("    請輸入想執行的選項: ");
+		fflush(stdin);
+		scanf("%c",&MENU);
+//_________________________________以上為第二題	
+		
+		switch(MENU)	//選單(多重選擇 ) 
+		{
+			case 'a':
+			case 'A':
+				system("CLS"); // 清除螢幕
+				do //do- while迴圈 
+				{
+					printf("    請輸入一個‘a’到‘n’的字元: ");		
+					fflush(stdin); 					// 使input buffer 淨空，常放在scanf()前;
+					scanf("%c",&c);
+					if(c < 'a' || c > 'n')				 
+ 		  		    {
+ 		  		    	printf("輸入錯誤!請重新輸入~\n");	//如果有錯，就要給一個提示字句告知輸入有錯。 
+					}
+				}
+				while(c < 'a' || c > 'n');		//判斷輸入值是否在a~n之間，是的話往下做，不是的話回到do下方再做一次 
+				for(A= c ; A>='a' ;A-- )	 	//迴圈(A存入剛剛輸入的值，如果A大於等於'a'，做A--) 
+				{
+					for (space ='a' ; space <A ; space++)//讓輸出可以靠又對齊排列，加上適當的空格 
+					{
+            			printf(" ");
+        			}
+					for( B = A ; B <= c ; B++ )//輸出時以升冪排列 
+					{
+						printf("%c",B);			
+					}
+					printf("\n");		
+				}
+    	    	getch();						// 按任意鍵清除 
+				system("CLS"); 					// 清除螢幕			
+				break;
+//_________________________________以上為第三題					
+			case 'b':
+			case 'B':
+				system("cls"); // 清除螢幕
+				do//do-while迴圈 
+				{				
+ 	  	    	 	printf("請輸入一個1至9的整數n：");
+ 	 	     	 	fflush(stdin); 					// 使input buffer 淨空，常放在scanf()前;
+ 	  			    scanf("%d", &n);
+ 	  			    if(n < 1 || n > 9)		//如果有錯，就要給一個提示字句告知輸入有錯。 
+ 	 	 		    {
+ 	 	 		    	printf("輸入錯誤!請重新輸入~\n");	
+					}
+    			}
+    	   		while(n < 1 || n > 9);//判斷輸入值是否在 1~9 之間 ，沒有就重做，有就往下做 
+     	  		printf("乘法表：\n");
+      	  		for (i = 1; i <= n; i++) 
+						{
+      			      	for (j = 1; j <= n; j++) 
+						{
+      			          printf("  %d * %d = %2d  ",i,j, i * j);//保留適當的空間有利排版 
+      			      	}
+      			     	printf("\n");
+       	 		}
+        		getch();						// 按任意鍵清除 
+				system("CLS"); 					// 清除螢幕
+				break;
+//_________________________________以上為第四題	
+			case 'c':
+			case 'C':
+				do//do-while迴圈 
+				{
+					printf("請問是否要繼續執行程式? [是] 請輸入(Y、y)，[否] 請輸入(N、n) :\n"); 
+					fflush(stdin);
+					scanf("%c",&OPTION);
+					if(OPTION=='y'||OPTION=='Y')		//如果輸入Y或是y 
+					{
+						getch();						// 按任意鍵清除 
+						system("CLS"); 					// 清除螢幕
+						break;							//跳出case 
+					}
+					else if(OPTION=='n'||OPTION=='N')	//如果輸入N或是n 
+					{
+						return 0;//結束程式 
+					}
+					else //if( (OPTION!='y'||OPTION!='Y') || (OPTION!='n'||OPTION!='N') )
+					{
+					 	printf("輸入錯誤\n");//都不是就出現提示字句 
+					}
+				}
+				while( (OPTION!='y'||OPTION!='Y') || (OPTION!='n'||OPTION!='N') );//當輸入不等於 { Y,y,N,n }就回到do再做一次 
+		}
+	}
+	system("PAUSE");
+	return 0;
+//_________________________________以上為第五題	
+}
+/***************以下是副程式********************/ 
+void picture(void)//個人化面的內容輸出 
+{
 	printf(" 																	      \n");
 	printf("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   \n");
 	printf(" 																	      \n");
@@ -29,145 +161,27 @@ int main (void)
 	printf("   																	      \n");
 	printf("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   \n");
 	printf(" 																	      \n");
-	//system("pause");             // 螢幕畫面暫停，並等待使用者按任意鍵
-	//system("CLS");               // 清除螢幕
-	while (count < 3) 
-	{
-        printf("請輸入四個數字的密碼：");
-        scanf("%d",&password);
-        count++;
-        if (password==2023)
-		{
-            printf("輸入正確！歡迎~\n");
-            break;
-        } 
-		else 
-		{
-	        printf("密碼錯誤，已輸入: %d 次。\n", count);
-            if (count == 3) 
-			{
-        		printf("輸入已連續錯誤三次，登入結束！\n");
-      		  printf("%c", '\a');					//輸出警告聲響 
-				return 0;
-    		}
-    	}
-    }
-//_________________________________以上為第一題	
-	system("CLS");               // 清除螢幕
-	//印出主選單 
-	
-	while(1)
-	{
-		{
-			printf("\n");
-			printf("\n");
-			printf("          __________________________________ \n");
-			printf("         |                                  |\n");
-			printf("         |    *****                  *      |\n");/*螢幕出現主選單，*/ 
-			printf("         |     ***  E3A 50  陳昱宏  ***     |\n");
-			printf("         |      *                  *****    |\n");
-			printf("         |__________________________________|\n");
-			printf("         |                                  |\n");
-			printf("         |      *        MENU        *      |\n");
-			printf("         |                                  |\n");
-			printf("         |         a. 畫出直角三角形        |\n");
-			printf("         |         b. 顯示乘法表            |\n");
-			printf("         |         c. 結束                  |\n");
-			printf("         |                                  |\n");
-			printf("         |                                  |\n");
-			printf("         |__________________________________|\n");
-		}
-	
-		printf("    請輸入想執行的選項: ");
-		fflush(stdin);
-		scanf("%c",&MENU);
-//_________________________________以上為第二題	
-		
-		switch(MENU)	//選單(多重選擇 ) 
-		{
-			case 'a':
-			case 'A':
-				system("CLS"); // 清除螢幕
-				do 
-				{
-					printf("    請輸入一個‘a’到‘n’的字元: ");		
-					fflush(stdin); 					// 使input buffer 淨空，常放在scanf()前;
-					scanf("%c",&c);
-					if(c < 'a' || c > 'n')				 
- 		  		    {
- 		  		    	printf("輸入錯誤!請重新輸入~\n");	//如果有錯，就要給一個提示字句告知輸入有錯。 
-					}
-				}
-				while(c < 'a' || c > 'n');
-				for(A= c ; A>='a' ;A-- )	 
-				{
-					for (space ='a' ; space <A ; space++)//讓輸出可以靠又對齊排列，加上適當的空格 
-					{
-            			printf(" ");
-        			}
-					for( B = A ; B <= c ; B++ )//輸出時以升冪排列 
-					{
-						printf("%c",B);			
-					}
-					printf("\n");		
-				}
-    	    	getch();						// 按任意鍵清除 
-				system("CLS"); 					// 清除螢幕			
-				break;
-//_________________________________以上為第三題					
-			case 'b':
-			case 'B':
-				system("cls"); // 清除螢幕
-				do
-				{				
- 	  	    	 	printf("請輸入一個1至9的整數n：");
- 	 	     	 	fflush(stdin); 					// 使input buffer 淨空，常放在scanf()前;
- 	  			    scanf("%d", &n);
- 	  			    if(n < 1 || n > 9)		//如果有錯，就要給一個提示字句告知輸入有錯。 
- 	 	 		    {
- 	 	 		    	printf("輸入錯誤!請重新輸入~\n");	
-					}
-    			}
-    	   		while(n < 1 || n > 9);
-     	  		printf("乘法表：\n");
-      	  		for (i = 1; i <= n; i++) 
-						{
-      			      	for (j = 1; j <= n; j++) 
-						{
-      			          printf("  %d * %d = %2d  ",i,j, i * j);//排版注意空間 
-      			      	}
-      			     	printf("\n");
-       	 		}
-        		getch();						// 按任意鍵清除 
-				system("CLS"); 					// 清除螢幕
-				break;
-//_________________________________以上為第四題	
-			case 'c':
-			case 'C':
-				do
-				{
-					printf("請問是否要繼續執行程式? [是] 請輸入(Y、y)，[否] 請輸入(N、n) :\n"); 
-					fflush(stdin);
-					scanf("%c",&OPTION);
-					if(OPTION=='y'||OPTION=='Y')
-					{
-						getch();						// 按任意鍵清除 
-						system("CLS"); 					// 清除螢幕
-						break;							//跳出case 
-					}
-					else if(OPTION=='n'||OPTION=='N')
-					{
-						return 0;
-					}
-					else //if( (OPTION!='y'||OPTION!='Y') || (OPTION!='n'||OPTION!='N') )
-					{
-					 	printf("輸入錯誤\n");
-					}
-				}
-				while( (OPTION!='y'||OPTION!='Y') || (OPTION!='n'||OPTION!='N') );
-		}
-	}
-	system("PAUSE");
-	return 0;
-//_________________________________以上為第五題	
+	return;
+}
+/*____________________________________________*/ 
+void menu(void)//輸出主選單的圖表 
+{
+	printf("\n");
+	printf("\n");
+	printf("          __________________________________ \n");
+	printf("         |                                  |\n");
+	printf("         |    *****                  *      |\n");/*螢幕出現主選單，*/ 
+	printf("         |     ***  E3A 50  陳昱宏  ***     |\n");
+	printf("         |      *                  *****    |\n");
+	printf("         |__________________________________|\n");
+	printf("         |                                  |\n");
+	printf("         |      *        MENU        *      |\n");
+	printf("         |                                  |\n");
+	printf("         |         a. 畫出直角三角形        |\n");
+	printf("         |         b. 顯示乘法表            |\n");
+	printf("         |         c. 結束                  |\n");
+	printf("         |                                  |\n");
+	printf("         |                                  |\n");
+	printf("         |__________________________________|\n");
+	return;
 }
